59db34ed6c1d35bc78313604c0bc05fb929151c0
Jacob Adams <jdadams@utah.gov>
2020-05-18 17:21:59 -0600

    secrets template and git ignore

---
+++ fleetshare_secrets_template.py
@@ -0,0 +1,12 @@
+#: Path to csv from fleet
+CSV_PATH =''
+#: AGOL username
+USERNAME = ''
+#: AGOL password
+PASSWORD = ''
+#: URL to service definition item in AGOL
+SD_ITEM_ID = ''
+#: URL to features item in AGOL
+FEATURES_ITEM_ID = ''
+#: path to ArcGIS Pro project for staging the service
+PROJECT_PATH = ''
