70eed599b1ef8475eb7389766d65bd38cd4d7200
Jacob Adams <jdadams@utah.gov>
2020-05-20 11:29:25 -0600

    feature service name must match existing service

--- fleetshare_secrets_template.py
+++ fleetshare_secrets_template.py
@@ -1,9 +1,15 @@
 #: Path to csv from fleet
 CSV_PATH = ''
+#: Name of published hosted feature service on AGOL 
+FEATURE_SERVICE_NAME = ''
+#: SFTP username
+SFTP_USERNAME = ''
+#: SFTP password
+SFTP_PASSWORD = ''
 #: AGOL username
-USERNAME = ''
+AGOL_USERNAME = ''
 #: AGOL password
-PASSWORD = ''
+AGOL_PASSWORD = ''
 #: URL to service definition item in AGOL
 SD_ITEM_ID = ''
 #: URL to features item in AGOL
