b36e2443c0476ec1ff0bb2a4023aa879310e6bc8
Jacob Adams <jdadams@utah.gov>
2020-06-08 10:33:18 -0600

    line length

--- update_agol_vehicles_pallet.py
+++ update_agol_vehicles_pallet.py
@@ -28,7 +28,9 @@ def get_latest_csv(temp_csv_dir, limit_three_days=False):
     #: Pull the date out of vehicle_data_yyyymmdd.csv to check recency
     date_string = str(latest_csv).rsplit('_')[-1].split('.')[0]
     try:
-        csv_datetime = datetime.date(int(date_string[:4]), int(date_string[4:6]), int(date_string[6:]))
+        csv_datetime = datetime.date(int(date_string[:4]),
+                                     int(date_string[4:6]),
+                                     int(date_string[6:]))
     except ValueError as e:
         raise e
 
@@ -69,7 +71,8 @@ def get_map_layer(project_path, fc_to_add):
     return layer, sharing_map
 
 
-def update_agol_feature_service(sharing_map, layer, feature_service_name, sddraft_path, sd_path, sd_item):
+def update_agol_feature_service(sharing_map, layer, feature_service_name,
+                                sddraft_path, sd_path, sd_item):
     '''
     Helper method for updating an AGOL hosted feature service from an ArcGIS
     Pro arcpy.mp.Map and .Layer ojbects.
@@ -88,7 +91,8 @@ def update_agol_feature_service(sharing_map, layer, feature_service_name, sddraf
                             service.
     '''
 
-    sharing_draft = sharing_map.getWebLayerSharingDraft('HOSTING_SERVER', 'FEATURE', feature_service_name, [layer])
+    sharing_draft = sharing_map.getWebLayerSharingDraft(
+        'HOSTING_SERVER', 'FEATURE', feature_service_name, [layer])
     sharing_draft.exportToSDDraft(sddraft_path)
     arcpy.server.StageService(sddraft_path, sd_path)
     sd_item.update(data=sd_path)
@@ -96,11 +100,14 @@ def update_agol_feature_service(sharing_map, layer, feature_service_name, sddraf
 
 
 def process():
+    
+    #: Set up paths and directories
     feature_service_name = secrets.FEATURE_SERVICE_NAME
 
     temp_csv_dir = os.path.join(arcpy.env.scratchFolder, 'fleet')
     temp_fc_path = os.path.join(arcpy.env.scratchGDB, feature_service_name)
-    sddraft_path = os.path.join(arcpy.env.scratchFolder, f'{feature_service_name}.sddraft')
+    sddraft_path = os.path.join(arcpy.env.scratchFolder, 
+                                f'{feature_service_name}.sddraft')
     sd_path = sddraft_path[:-5]
 
     paths = [temp_csv_dir, temp_fc_path, sddraft_path, sd_path]
@@ -111,6 +118,7 @@ def process():
     os.mkdir(temp_csv_dir)
 
     #: Download all the files in the upload folder on sftp to temp_csv_dir
+    print(f'Downloading all files from {secrets.KNOWNHOSTS}/upload...')
     connection_opts = pysftp.CnOpts(knownhosts=secrets.KNOWNHOSTS)
     with pysftp.Connection(secrets.SFTP_HOST, 
                            username=secrets.SFTP_USERNAME, 
@@ -121,11 +129,16 @@ def process():
     source_path = str(get_latest_csv(temp_csv_dir))
 
     print(f'Converting {source_path} to feature class {temp_fc_path}...')
-    result = arcpy.management.XYTableToPoint(source_path, temp_fc_path, 'LONGITUDE', 'LATITUDE', coordinate_system=arcpy.SpatialReference(4326))
+    wgs84 = arcpy.SpatialReference(4326)
+    result = arcpy.management.XYTableToPoint(source_path, temp_fc_path, 
+                                             'LONGITUDE', 'LATITUDE', 
+                                             coordinate_system=wgs84)
 
     #: Overwrite existing AGOL service
     print(f'Connecting to AGOL as {secrets.AGOL_USERNAME}...')
-    gis = arcgis.gis.GIS('https://www.arcgis.com', secrets.AGOL_USERNAME, secrets.AGOL_PASSWORD)
+    gis = arcgis.gis.GIS('https://www.arcgis.com', 
+                         secrets.AGOL_USERNAME, 
+                         secrets.AGOL_PASSWORD)
     sd_item = gis.content.get(secrets.SD_ITEM_ID)
 
     print('Getting map and layer...')
@@ -133,7 +146,8 @@ def process():
 
     #: draft, stage, update, publish
     print(f'Staging and updating...')
-    update_agol_feature_service(fleet_map, layer, feature_service_name, sddraft_path, sd_path, sd_item)
+    update_agol_feature_service(fleet_map, layer, feature_service_name,
+                                sddraft_path, sd_path, sd_item)
 
     #: Update item description
     print('Updating item description...')
