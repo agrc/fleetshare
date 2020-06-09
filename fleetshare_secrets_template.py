2b7c1725cd9b12de66482ae3f6d45a6d6f11c05e
Jacob Adams <jdadams@utah.gov>
2020-06-09 14:32:17 -0600

    docstring for secrets template

--- fleetshare_secrets_template.py
+++ fleetshare_secrets_template.py
@@ -1,3 +1,13 @@
+'''
+fleetshare_secrets_template.py:
+Template secrets file for the update_agol_vehicles_pallet script. Copy this
+file to 'fleetshare_secrets.py' and add proper values for each item.
+
+This file IS tracked in version control; do not add sensitive data to this file.
+Use the copy named 'fleetshare_secrets.py', which is excluded via the project's
+.gitignore file. Verify that your local version control is excluding the copy.
+'''
+
 #: Path to csv from fleet
 CSV_PATH = ''
 #: Name of published hosted feature service on AGOL
