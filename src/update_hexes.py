e6abc6ffa81012cb7c2b71f33f8001d6db0c13fa
Jake Adams <jdadams@utah.gov>
2021-02-11 13:20:05 -0700

    fix: refactor dhrm df, operator eins

--- src/update_hexes.py
+++ src/update_hexes.py
@@ -9,16 +9,16 @@ import pandas as pd
 import arcpy
 
 
-def get_wfh_eins(survey_path, monthly_employee_data_path, output_csv_path):
+def get_wfh_eins(survey_path, monthly_dhrm_data, output_csv_path):
     '''Create a csv of the employee data that have matching records in the WFH survey
 
     Args:
-        survey_path (Path): xls/x file of the WFH survey data
-        monthly_employee_data_path (Path): xls/x of employee data
+        survey_path (Path): xlsx file of the WFH survey data
+        monthly_dhrm_data (DataFrame): monthly employee data
         output_csv_path (Path): output csv file
     '''
 
-    print('Reading WFH survey...')
+    print(f'\nReading WFH survey {survey_path}...')
     survey_df = pd.read_excel(survey_path, engine='openpyxl')
 
     #: Drop double header row
@@ -34,8 +34,29 @@ def get_wfh_eins(survey_path, monthly_employee_data_path, output_csv_path):
     wfh_eins_int = eins[eins.str.len() == 6].astype(int)
     wfh_eins_int = wfh_eins_int.drop_duplicates()
 
-    #: Format the monthly data from DHRM
-    print('Reading DHRM employee data...')
+    #: "join" by only including rows where EINint is in series of EINs from WFH survey
+    wfh_records = monthly_dhrm_data[monthly_dhrm_data['EINint'].isin(wfh_eins_int)]
+    print(
+        f'Employee records with matching EIN in WFH survey: {wfh_records.shape[0]} (out of {wfh_eins_int.count()} EINs from survey)'
+    )
+
+    print(f'Saving output data to {output_csv_path}...')
+    wfh_records.to_csv(output_csv_path)
+
+
+def get_dhrm_dataframe(monthly_employee_data_path):
+    '''Read and process monthly DHRM employee data dump.
+
+    Creates real_addr/real_zip field with mailing address/zip if provided, physical address/zip otherwise.
+    Creates EINint field by converting EIN to int, ignoring any errors
+
+    Args:
+        monthly_employee_data_path (Path): xls file from DHRM
+
+    Returns:
+        DataFrame: DHRM data with real_addr, real_zip, and EINint fields added.
+    '''
+    print(f'\nReading DHRM employee data {monthly_employee_data_path}...')
     monthly_df = pd.read_excel(monthly_employee_data_path)
     #: Use different row for header
     monthly_df.columns = monthly_df.iloc[0]
@@ -43,7 +64,6 @@ def get_wfh_eins(survey_path, monthly_employee_data_path, output_csv_path):
     monthly_df = monthly_df.drop(monthly_df.index[0])
     #: Drop "Page" row at bottom
     monthly_df = monthly_df.drop(monthly_df.index[-1])
-
     #: Use physical addr if available, otherwise stick with mailing (improves geocode by 1% :shrug:)
     monthly_df['real_addr'] = np.where(
         monthly_df['physical_address_line1'].isnull(), monthly_df['mailing_address_line1'],
@@ -53,24 +73,10 @@ def get_wfh_eins(survey_path, monthly_employee_data_path, output_csv_path):
         monthly_df['Empl Physical ZIP'].isnull(), monthly_df['Empl Mail ZIP'].str.strip().str.slice(stop=5),
         monthly_df['Empl Physical ZIP'].str.strip().str.slice(stop=5)
     )
-
     #: Convert EINs to ints
     monthly_df['EINint'] = monthly_df['EIN'].astype(int, errors='ignore')
 
-    #: "join" by only including rows where EINint is in series of EINs from WFH survey
-    wfh_records = monthly_df[monthly_df['EINint'].isin(wfh_eins_int)]
-    print(
-        f'Employee records with matching EIN in WFH survey: {wfh_records.shape[0]} (out of {wfh_eins_int.count()} EINs from survey)'
-    )
-
-    # operators_path = r"A:\telework_survey\Operators.xlsx"
-    # operators_df = pd.read_excel(operators_path, engine='openpyxl')
-    # cleaned_operators = operators_df[(operators_df['EIN']>=100000) & (operators_df['EIN'] <= 999999)]
-    # op_merged = pd.merge(monthly_df, cleaned_operators, how='inner', left_on='EINint', right_on='EIN')
-    # op_merged.to_csv(r'A:\telework_survey\operator_records.csv')
-
-    print(f'Saving output data to {output_csv_path}...')
-    wfh_records.to_csv(output_csv_path)
+    return monthly_df
 
 
 def geocode_points(points_csv, out_fc, locator, addr_field, zip_field):
@@ -84,39 +90,62 @@ def geocode_points(points_csv, out_fc, locator, addr_field, zip_field):
         zip_field (str): The zip code field in points_csv
     '''
 
-    #: using 'memory' seems to limit the geocode to 1000
-    # geocode_fc = r'A:\telework_survey\wfh.gdb\geocoded_test_intermediate'
-    geocode_fc = r'memory\temp_geocode_fc'
+    #: using 'memory' seems to limit the geocode to 1000, use 'in_memory' instead.
+    geocode_fc = r'in_memory\temp_geocode_fc'
+
+    if arcpy.Exists(geocode_fc):
+        arcpy.management.Delete(geocode_fc)
 
     fields_str = f"'Street or Intersection' {addr_field} VISIBLE NONE;'City or Placename' <None> VISIBLE NONE;'ZIP Code' {zip_field} VISIBLE NONE"
-    print('Geocoding (this could take a while)...')
+    print(f'\nGeocoding {points_csv} (this could take a while)...')
     geocode_results = arcpy.geocoding.GeocodeAddresses(points_csv, locator, fields_str, geocode_fc)
 
     print(geocode_results.getMessages())
 
     print('Copying out only the matched points...')
     query = "Status = 'M'"
+
+    if arcpy.Exists(geocode_fc):
+        arcpy.management.Delete('geocode_layer')
+
     arcpy.management.MakeFeatureLayer(geocode_fc, 'geocode_layer', query)
     arcpy.management.CopyFeatures('geocode_layer', out_fc)
 
-    #: Testing environment stuff
-    environments = arcpy.ListEnvironments()
 
-    # Sort the environment names
-    environments.sort()
+def get_operator_eins(operators_path, monthly_dhrm_data, output_csv_path):
+    '''Read and process approved operator data
 
-    for environment in environments:
-        # Format and print each environment and its current setting.
-        # (The environments are accessed by key from arcpy.env.)
-        print("{0:<30}: {1}".format(environment, arcpy.env[environment]))
+    Args:
+        operators_path (Path): xlsx of approved operators from Fleet
+        monthly_dhrm_data (DataFrame): Monthly DHRM data
+        out_path (Path): output csv file
+    '''
+    print(f'\nReading approved operator data {operators_path}...')
+    operators_df = pd.read_excel(operators_path, engine='openpyxl')
+    cleaned_operators = operators_df[(operators_df['EIN'] >= 100000) & (operators_df['EIN'] <= 999999)]
+    op_merged = pd.merge(monthly_dhrm_data, cleaned_operators, how='inner', left_on='EINint', right_on='EIN')
+    print(f'Saving output data to {output_csv_path}...')
+    op_merged.to_csv(output_csv_path)
 
 
 if __name__ == '__main__':
-    survey_path = Path(r'A:\telework_survey\Teleworking Onboarding Survey_December 14, 2020_08.45.xlsx')
-    employee_data_path = Path(r'A:\monthly_data\2020_12_01.xls')
-    out_path = Path(r'A:\telework_survey\wfh_records_test.csv')
-    geocoded_points_path = Path(r'A:\telework_survey\wfh.gdb\geocoded_test')
+    employee_data_path = Path(r'A:\monthly_data\2021_02_01.xls')
+
+    wfh_survey_data_path = Path(r'A:\telework_survey\Teleworking Onboarding Survey_December 14, 2020_08.45.xlsx')
+    wfh_csv_out_path = Path(r'A:\telework_survey\wfh_records_test.csv')
+    wfh_geocoded_points_path = Path(r'A:\telework_survey\wfh.gdb\wfh_geocoded')
+
+    operator_data_path = Path(r"A:\telework_survey\Operators.xlsx")
+    operator_csv_out_path = Path(r'A:\telework_survey\operator_records_test.csv')
+    operator_geocoded_points_path = Path(r'A:\telework_survey\wfh.gdb\operator_geocoded')
+
     locator_path = Path(r'C:\temp\locators\AGRC_CompositeLocator.loc')
 
-    # get_wfh_eins(survey_path, employee_data_path, out_path)
-    geocode_points(str(out_path), str(geocoded_points_path), str(locator_path), 'real_addr', 'real_zip')
+    dhrm_data = get_dhrm_dataframe(employee_data_path)
+
+    get_wfh_eins(wfh_survey_data_path, dhrm_data, wfh_csv_out_path)
+    get_operator_eins(operator_data_path, dhrm_data, operator_csv_out_path)
+    geocode_points(str(wfh_csv_out_path), str(wfh_geocoded_points_path), str(locator_path), 'real_addr', 'real_zip')
+    geocode_points(
+        str(operator_csv_out_path), str(operator_geocoded_points_path), str(locator_path), 'real_addr', 'real_zip'
+    )
